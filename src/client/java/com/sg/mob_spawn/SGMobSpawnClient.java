b154c8c6223e4cd44dfef17486263dd32988c4d4
ArielM24 <ariel_montoya15@outlook.es>
2025-05-21 10:44:35 -0600

    init

---
+++ src/client/java/com/sg/mob_spawn/SGMobSpawnClient.java
@@ -0,0 +1,10 @@
+package com.sg.mob_spawn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SGMobSpawnClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
